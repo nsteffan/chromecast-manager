62d0686fb5bae0c46223ac6695d7e3c831fc68b8
Nate Steffan <nate@natesteffan.com>
2016-08-03 13:28:14 -0500

    Initial Chromecast device handler

---
+++ devicetypes/nsteffan/chromecast-device-handler.src/chromecast-device-handler.groovy
@@ -0,0 +1,110 @@
+/**
+ *  Chromecast Device Handler
+ *
+ *  Copyright 2016 Nate Steffan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Chromecast Device Handler", namespace: "nsteffan", author: "Nate Steffan", oauth: true) {
+		capability "Actuator"
+		capability "Switch"
+		capability "Refresh"
+		capability "Sensor"
+		capability "Music Player"
+        
+        attribute "model", "string"
+		attribute "trackUri", "string"
+		attribute "transportUri", "string"
+		attribute "trackNumber", "string"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'status' attribute
+	// TODO: handle 'level' attribute
+	// TODO: handle 'trackDescription' attribute
+	// TODO: handle 'trackData' attribute
+	// TODO: handle 'mute' attribute
+
+}
+
+// handle commands
+def play() {
+	log.debug "Executing 'play'"
+	// TODO: handle 'play' command
+}
+
+def pause() {
+	log.debug "Executing 'pause'"
+	// TODO: handle 'pause' command
+}
+
+def stop() {
+	log.debug "Executing 'stop'"
+	// TODO: handle 'stop' command
+}
+
+def nextTrack() {
+	log.debug "Executing 'nextTrack'"
+	// TODO: handle 'nextTrack' command
+}
+
+def playTrack() {
+	log.debug "Executing 'playTrack'"
+	// TODO: handle 'playTrack' command
+}
+
+def setLevel() {
+	log.debug "Executing 'setLevel'"
+	// TODO: handle 'setLevel' command
+}
+
+def mute() {
+	log.debug "Executing 'mute'"
+	// TODO: handle 'mute' command
+}
+
+def previousTrack() {
+	log.debug "Executing 'previousTrack'"
+	// TODO: handle 'previousTrack' command
+}
+
+def unmute() {
+	log.debug "Executing 'unmute'"
+	// TODO: handle 'unmute' command
+}
+
+def setTrack() {
+	log.debug "Executing 'setTrack'"
+	// TODO: handle 'setTrack' command
+}
+
+def resumeTrack() {
+	log.debug "Executing 'resumeTrack'"
+	// TODO: handle 'resumeTrack' command
+}
+
+def restoreTrack() {
+	log.debug "Executing 'restoreTrack'"
+	// TODO: handle 'restoreTrack' command
+}
